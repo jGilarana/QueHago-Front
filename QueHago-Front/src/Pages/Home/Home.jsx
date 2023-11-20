e972eb711f6b43c258a372d8c487088969e2630c
jGilarana <jorgereboot@gmail.com>
2023-11-20 01:26:37 +0000

    frontend comenzado

--- QueHago-Front/src/Pages/Home/Home.jsx
+++ QueHago-Front/src/Pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import  { React, useEffect, useState } from 'react'
 import { getAllEvents } from '../../Services/eventService'
+import { Button } from '@mui/material'
+import { green, red } from '@mui/material/colors'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
 const [event, setEvent] = useState([])
 
 const getEvents = async() => {
-
   const data = await getAllEvents()
-  console.log(data)
   setEvent(data)
 }
 
@@ -19,7 +20,14 @@ useEffect(() => {
 
   return (
     <>
+     <Link to={'/login'}><Button sx={{backgroundColor: green[600]}} color="success">
+              Login
+            </Button></Link>
+            <Link to={'/signup'}><Button sx={{backgroundColor: red[600]}} color="success">
+              SignUp
+            </Button></Link>
      {event.map((em, i) => (<h1 key={i}>{em.title}</h1>))}
+     
     </>
   )
 }
